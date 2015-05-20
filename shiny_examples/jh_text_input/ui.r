3fbc7cced42ec3c55137d2c0e6e4f022127dc1b0
superjohn_ca <johnkirkhoughton1@gmail.com>
2015-05-20 13:42:44 -0700

    DELETE EVERYTHING!!!!

--- shiny_examples/jh_text_input/ui.r
+++
@@ -1,9 +0,0 @@
-shinyUI(fluidPage(
-  
-  # Copy the line below to make a text input box
-  textInput("text", label = h3("Enter Action IDs Here"), value = "20525217,20524903"),
-  
-  hr(),
-  fluidRow(column(3, verbatimTextOutput("value")))
-  
-))
